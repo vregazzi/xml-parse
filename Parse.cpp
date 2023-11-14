1508e0172541c238a828f07a544e92f4ab17036a
Vincent Regazzi <vincentregazzi@gmail.com>
2023-11-13 18:39:31 -0600

    make script to parse xml file

---
+++ Parse.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "tinyxml2/tinyxml2.cpp"
+
+void PrintTitles(std::string file)
+{
+    tinyxml2::XMLDocument doc;
+    doc.LoadFile(file.c_str());
+
+    tinyxml2::XMLElement *root = doc.RootElement();
+
+    for (tinyxml2::XMLElement *child = root->FirstChildElement(); child != NULL; child = child->NextSiblingElement())
+    {
+        for (tinyxml2::XMLElement *title = child->FirstChildElement();
+             title != NULL; title = title->NextSiblingElement())
+        {
+            if (title->Name() == std::string("title"))
+            {
+
+                std::cout << title->GetText() << '\n';
+            }
+        }
+    }
+    return;
+}
+
+int main()
+{
+    std::string fileName;
+    std::cout << "Enter file name: ";
+    std::cin >> fileName;
+
+    PrintTitles(fileName);
+
+    return 0;
+}
